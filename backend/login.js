1a52ecb6ed148d3ad358bec6b36ecdc284f05eb3
Yizhun Sim <sim.yizhun@gmail.com>
2021-11-28 01:54:31 +0800

    Implement Login function to pass in account_id

---
+++ backend/login.js
@@ -0,0 +1,71 @@
+// Import database file to use the established mysql connection.
+const database = require("./database");
+
+// Import express to define URI mappings
+const express = require("express");
+
+// Define a router object which will hold the URI mappings.
+router = express.Router();
+
+// LOGIIN
+router.post('/login', async (req, res) => {
+    var username = req.body.username;
+	var password = req.body.password;
+
+    let user, userJoinAccount;
+    //Validate the data before we accept login request
+    console.log(req.body);
+
+    getUserIdAccount();
+
+    function getUserIdAccount(){
+        var query1 = `select * from user
+        where user_email = "${username}"`;
+
+        database.connection.query(query1, async (error, result) => {
+            console.log(query1);
+            if (error){
+                console.log(`getUserIdAccount: Unable to find account that is tagged to user account. Error: ${error}`);
+                response.status(500).send(`Unable to login with account: ${request.query.account_id}`);
+            }else{
+                user = await result;
+                console.log(`getUserAccount: ${JSON.stringify(user)}`);
+
+                if (!user) {
+                    return res.status(400).send("Email does not exist!"); //TO change to email or password doesnt exist
+                }else{
+                    await getAccountId();
+                    const validPass = password == user[0].user_password;
+                    // Check if password is CORRECT
+                    if (!validPass) {
+                        return res.status(400).send("Invalid password"); //TO change to email or password doesnt exist
+                    }
+                  }
+            }
+        })
+    }
+
+   async function getAccountId(){
+        var query2 = `select * from account left join user on account.user_id = user.user_id where account.user_id = ${user[0].user_id}`;
+        database.connection.query(query2, async (error, result) => {
+            console.log(query2);
+            if (error){
+                console.log(`getAccountID: Unable to find account that is tagged to user account. Error: ${error}`);
+                response.status(500).send(`Unable to login with account: ${username}`);
+            }else{
+                userJoinAccount = await result;
+                console.log(`getUserAccount: ${JSON.stringify(userJoinAccount)}`);
+                res.header("auth-token", userJoinAccount[0].account_id).send("User successfully login");
+                //res.status(200).send("User successfully login");
+            }
+        })
+    }
+
+    // Create and assign a token
+    //const token = auth.genAccessToken(usersJoinAccount);
+
+  });
+
+  module.exports = {
+    router,
+};
--- backend/main.js
+++ backend/main.js
@@ -4,6 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // import cors to allow running server and client on the same system
 const cors = require("cors");
+// Import dotenv to import properties from .env file
+require("dotenv").config();
+const { auth } = require('express-openid-connect');
+
+const { requiresAuth } = require('express-openid-connect');
+
+const session = require('express-session');
+
+// create a service object which will listen to clients' requests
+service = express();
+
+service.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 // Import the code from user, account and transaction
 const user = require("./user");
@@ -13,10 +29,13 @@ const wallet_transaction = require("./wallet_transaction");
 //const wallet = require("./wallet.js");
 const pet = require("./pet");
 
-// create a service object which will listen to clients' requests
-service = express();
+const login = require("./login");
+
 // Tell the service to use JSON parser to parse requests.
 service.use(bodyParser.json());
+
+service.use(bodyParser.urlencoded({extended : true}));
+
 // Tell the service to use cors.
 service.use(cors());
 
@@ -24,14 +43,42 @@ service.use(cors());
 service.use(user.router);
 // Import API path mappings from account.js
 service.use(account.router);
-// Import API path mappings from transaction.js
-// service.use(transaction.router);
-// Import API path mappings from wallet.js
-//service.use(wallet.router);
 // Import API path mappings from wallet_transaction.js
 service.use(wallet_transaction.router);
-// Import API path mappings from pet.js
+// Import API path mappings from pet.j`
 service.use(pet.router);
 
-// Start the service at port number 3000
-service.listen(3000);
+service.use(login.router);
+
+service.set('trust proxy', 1);
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+service.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+service.get('/', (req, res) => {
+    // console.log(req.oidc.isAuthenticated());
+
+    //   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+service.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
+const port = process.env.PORT || 3000;
+service.listen(port,() => {
+    console.log(`Listening on port ${port}`);
+});
+
