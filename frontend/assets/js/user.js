1d6d4f7588d329b1629df5fd0be6851bbeda2779
mcsenget <42573859+benchiatc@users.noreply.github.com>
2021-11-28 20:01:03 +0800

    commit

--- frontend/assets/js/user.js
+++ frontend/assets/js/user.js
@@ -1,3 +1,5 @@
+var account_id = 1;
+
 function getUsers() {
     fetch("http://localhost:3000/user/all", {method: "GET"})
         .then((response) => response.json())
@@ -53,4 +55,20 @@ function getUserByUID() {
             $(".mypanel").html(text);
         })
         .catch((error) => console.log("error", error));
+}
+
+function petValue(){
+  console.log("account balance loaded");
+  fetch('http://localhost:3000//pet/by-aid?account_id'+ account_id, { method: 'GET' })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+      var text1 = `${data[].wallet_balance}`
+      var text2 = `${data[0].account_name}`
+      var text3 = `${data[0].wallet_id}`
+      $('#balanceId').html(text1+" CPM");
+      $('#accountId').html(text2);
+      $('#walletId').html(text3);
+    })
+    .catch((error) => console.log('error', error))
 }
