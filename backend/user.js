96813073faafb838873ad1cf2aff391b310b9d3b
Yizhun Sim <sim.yizhun@gmail.com>
2021-11-29 02:24:04 +0800

    POST user/add Insert user and account record concurrently

--- backend/user.js
+++ backend/user.js
@@ -1,5 +1,6 @@
 const database = require("./database");
 const express = require("express");
+const crypto = require("crypto");
 
 router = express.Router();
 
@@ -39,14 +40,45 @@ router.get("/user/by-uid", (request, response) => {
 router.post("/user/add", (request, response) =>{
     let user = request.body;
     console.log(`Request object to be sent out: ${JSON.stringify(user)}`);
-    database.connection.query(`insert into user (user_id, user_name, user_email, user_password, user_profile, user_update) values (null, "${user.user_name}", "${user.user_email}", "${user.password}", "${user.profile}", now());`, (error, records) =>{
-        if(error){
-            console.log(error)
-            response.status(500).send("Some error occured when adding a user");
-        }else{
-            response.status(200).send("User added to database successfully");
-        }
-    })
+
+    async function insertUserRecord(){
+        var query1 = `insert into user (user_id, user_name, user_email, user_password, user_profile, user_update) values (null, "${user.user_name}", "${user.user_email}", "${user.password}", "${user.profile}", now());`;
+        database.connection.query(query1, async (error, result) =>{
+            if(error){
+                console.log(error)
+                response.status(500).send("Some error occured when adding a user");
+            }else{
+                data = await result;
+                console.log(`getUserIdAccount: ${JSON.stringify(data)}`);
+                await addUserAccountRecord(data, user);
+            }
+        })
+    }
+
+    //insert into account (account_id, user_id, account_name, wallet_id, wallet_balance) values ('1', '1', 'Bobbe MacGahey', '0xa71fec3a8f3c73b172ada541578620db20fc1a51', '802.96');
+
+    async function addUserAccountRecord(data, user){
+        console.log(`I am here ${data.insertId}`);
+        console.log(`Request received: ${JSON.stringify(user)}`);
+        var walletId = crypto.randomBytes(20).toString('hex');
+        var query2 = `insert into account (account_id, user_id, account_name, wallet_id, wallet_balance)
+        values (null, "${data.insertId}", "${user.user_name}", "${walletId}", 0)`;
+
+        database.connection.query(query2, async (error, result) => {
+            console.log(query2);
+            if(error){
+                console.log(`Unable to add account for user: ${user.user_id}. Error: ${error}`);
+                response.status(500).send(`Unable to add user account: ${user.user_id}`);
+            }else{
+                data3 = await result;
+                console.log(`addUserAccount: ${JSON.stringify(data3)}`);
+                // console.log(`Added user and user account: ${request.query.account_id} associated with User: ${user.user_id}.`);
+                response.status(200).send("User added successfully");
+            }
+        })
+    }
+
+    insertUserRecord();
 })
 
 // Define a mapping for a DELETE request on API path /account/by-uid
