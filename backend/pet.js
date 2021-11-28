992835a786d862fde2a58ebca7d3583fef09ad86
Yizhun Sim <sim.yizhun@gmail.com>
2021-11-28 20:42:11 +0800

    Add GET pet_contribution by pet_id

--- backend/pet.js
+++ backend/pet.js
@@ -67,6 +67,36 @@ router.get("/pet/by-pid", (request, response) => {
     );
 });
 
+// Define a mapping for a GET request on API path /pet_contribution/by-pid
+// to an arrow function which requires a parameter named id from the request
+// and calls a mysql query and populates the response with the data.
+router.get("/pet_contribution/by-pid", (request, response) => {
+    // Before making a query, validate the id received from the request.
+    // If the id is empty or if the id is not a number, then return a "bad request" response.
+    if (request.query.pet_id.length === 0 || isNaN(request.query.pet_id)) {
+        console.log(`Invalid ID received. ID: ${request.query.pet_id}`);
+        response.status(400).send("Invalid ID received.");
+        return;
+    }
+    // After the validation check, call a mysql query to request data from account using the account id received in the request.
+    database.connection.query(
+        `select *
+         from pet_calendar
+         where pet_id = ${request.query.pet_id}`,
+        (errors, results) => {
+            // if the query failed, return a "failure" message to the frontend client.
+            if (errors) {
+                console.log(errors);
+                response.status(500).send("Internal Serve Error"); // status(500) sets the status code to 500, which means some error occurred in the server.
+            }
+            // Otherwise, populate the response with the results.
+            else {
+                response.status(200).send(results); // status(200) sets the status code to 200, which means response is OK.
+            }
+        }
+    );
+});
+
 // Insert into pet_calendar contribution amount
 // Update pet table total_cpm_paid
 
